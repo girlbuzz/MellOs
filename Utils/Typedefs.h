bb4226c44e6d504203d4c0b73005c772539464f1
minneelyyyy <minneelyyyy@proton.me>
2023-11-16 22:35:05 -0500

    minor fix: safer macro functions

--- Utils/Typedefs.h
+++ Utils/Typedefs.h
@@ -1,23 +1,21 @@
 #ifndef TYPEDEF_H
 #define TYPEDEF_H
 
-
-
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long size_t;
 
 typedef uint8_t bool;
-#define true    1
-#define false   0
+#define true    (1)
+#define false   (0)
 
-#define low_16(address) (uint16_t)((address) & 0xFFFF)
-#define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
-#define low_8(address) (uint8_t)((address) &  0xFF)
-#define high_8(address) (uint8_t)(((address) >> 8) & 0xFF)
+#define low_16(_LOW16_address) ((uint16_t)((_LOW16_address) & 0xFFFF))
+#define high_16(_HI16_address) ((uint16_t)(((_HI16_address) >> 16) & 0xFFFF))
+#define low_8(_LOW8_address) ((uint8_t)((_LOW8_address) &  0xFF))
+#define high_8(_HI8_address) ((uint8_t)(((_HI8_address) >> 8) & 0xFF))
 
-#define NULL (void*)0
+#define NULL ((void*)0)
 
 typedef int maybe_void;
 typedef int maybe_int;
--- Utils/assert.c
+++ Utils/assert.c
@@ -4,7 +4,7 @@
 extern char ker_tty[4000];
 
 void assert(int condition) {
-    if(!condition) {
+    if (!condition) {
         clear_tty(ERROR_COLOR, ker_tty);
         display_tty(ker_tty);
         SetCursorPosRaw(0);
